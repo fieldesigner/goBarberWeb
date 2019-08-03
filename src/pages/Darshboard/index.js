0d50c7c6c254420e6b95ff0d4bea7aaeec226ad3
Rafael Marques <fieldesigner@hotmail.com>
2019-08-03 14:39:38 -0300

    chore: Base do projeto goBarberWeb

---
+++ src/pages/Darshboard/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Darshboard() {
+  return (
+    <h1>Dashboard</h1>
+  );
+}
---
+++ src/routes/Route.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false; // variavel logado ou nao
+  
+  // se nao tiver logado e a rota for privada, redireciona para pag inicial
+  if(!signed && isPrivate){
+    return <Redirect to="/" />;
+  }
+
+  // se tiver logado e a rota nao for privada, redireciona para o dashboard
+  if(signed && !isPrivate){
+    return <Redirect to="/" />;
+  }
+
+  //definindo layout caso esteja logado ou nao
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return <Route {...rest} render={props => (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  )} />;
+  }
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
