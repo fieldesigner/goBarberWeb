86dd4f8ed76b688234cc68e0446b053d3400007a
Rafael Marques <fieldesigner@hotmail.com>
2019-08-05 15:50:53 -0300

    feature: Adiciona estilização no login e cadastro e Unform

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
+import {Form, Input} from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é Obrigatório'),
+  password:Yup.string()
+    .required('Senha obrigatória'),
+});
 
 export default function SingIn() {
+
+  function handleSubmit(data){
+    console.tron.log(data);
+  }
+
   return (
-    <h1>SingIn</h1>
+    <>
+    <img src={logo} alt="GoBarber" />
+
+    <Form schema={schema} onSubmit={handleSubmit}>
+      <Input name="email" type="email" placeholder="Seu email" />
+      <Input name="password" type="password" placeholder="Sua senha" />
+
+      <button type="submit">Acessar</button>
+      <Link to="/register">Criar conta</Link>
+    </Form>
+    </>
   );
 }
--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
+import {Form, Input} from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+  .required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é Obrigatório'),
+  password:Yup.string()
+    .min(6, 'Minimo de 6 caracteres')
+    .required('Senha obrigatória'),
+});
 
 export default function SingUp() {
+
+  function handleSubmit(data){
+    console.tron.log(data);
+  }
+
   return (
-    <div />
+    <>
+    <img src={logo} alt="GoBarber" />
+
+    <Form schema={schema} onSubmit={handleSubmit}>
+      <Input name="name" type="text" placeholder="Nome Completo" />
+      <Input name="email" type="email" placeholder="Seu email" />
+      <Input name="password" type="password" placeholder="Sua senha" />
+
+      <button type="submit">Crir conta</button>
+      <Link to="/">Já tenho uma conta</Link>
+    </Form>
+    </>
   );
 }
