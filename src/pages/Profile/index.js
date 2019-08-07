b9f656b78aa7c555b840ab91310a715b29619849
Rafael Marques <fieldesigner@hotmail.com>
2019-08-07 16:37:39 -0300

    feature: atualiza avatar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from "@rocketseat/unform";
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data){
+    dispatch(updateProfileRequest(data));
+  }
+
   return (
-    <div />
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+        <hr />
+        <Input name="oldPassword" type="password" placeholder="Senha aual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input name="confirmPassword" type="password" placeholder="Confirme nova senha" />
+        <button type="submit">Atualizar perfil</button>      
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,35 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }){
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+    console.tron.log('1');
+    const profile = Object.assign(
+        { name, email, avatar_id }, 
+        rest.oldPassword ? rest : {}
+      );
+      console.tron.log('2');
+    const response = yield call(api.put, 'users', profile);
+    console.tron.log('3');  
+    toast.success('Perfil atualizado com sucesso');
+    console.tron.log('4');
+    yield put(updateProfileSuccess(response.data));
+    console.tron.log('5');
+  } catch (err) {
+
+      toast.error('Erro ao atualizar');
+      yield put(updateProfileFailure());
+
+  }
+
+
+}
+
+export default all([
+  takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)
+]);
